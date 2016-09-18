33342fe23340f2b63ee0cee660ab07e564a5a1f6
Ingvi Rafn <ingvirafn@ingvirafn.com>
2016-09-18 14:54:49 +0000

    initial

---
+++ RealTimeTracerWeb/Startup.cs
@@ -0,0 +1,20 @@
+﻿using Owin;
+using Microsoft.Owin;
+using Microsoft.Owin.Cors;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RealTimeTracerWeb
+{
+    class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.UseCors(CorsOptions.AllowAll);
+            app.MapSignalR("/realtimetrace", new Microsoft.AspNet.SignalR.HubConfiguration());
+        }
+
+    }
+}
---
+++ RealTimeTracerWeb/TraceListenerHub.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNet.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Threading.Tasks;
+
+namespace RealTimeTracerWeb
+{
+    public class TraceListenerHub : Hub
+    {
+        public TraceListenerHub()
+        {
+
+        }
+
+        public override async Task OnConnected()
+        {
+            var channel = 
+                this.Context.QueryString["channel"];
+
+            if (String.IsNullOrEmpty(channel))
+            {
+                this.Context.Request.GetHttpContext().Response.End();
+                return;
+            }
+
+            Clients.Group(channel).onJoin(Context.ConnectionId);
+
+            await this.Groups.Add(Context.ConnectionId, channel);
+            await base.OnConnected();
+        }
+
+        public void Trace(string channel, string message)
+        {
+            if (!String.IsNullOrEmpty(channel))
+                Clients.Group(channel).onTrace(Context.ConnectionId, message);
+        }
+    }
+
+}
